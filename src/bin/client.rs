5d502a1f0a244a7768afa8c676b1e81677c6b7fd
Jude Packard-Jones <judepackardjones@gmail.com>
2023-12-19 12:38:54 -0500

    + comments

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -3,6 +3,8 @@ use crossterm::event::{poll, read, Event, KeyCode};
 use crossterm::terminal::enable_raw_mode;
 use mini_redis::client;
 use std::time::{Duration, Instant};
+use local_ip_address::local_ip;
+#[allow(unused_imports)]
 #[allow(dead_code)]
 #[derive(Debug)]
 enum Command {
@@ -14,38 +16,42 @@ enum Command {
 async fn main() {
     let fps = 60;
     // Establish a connection to the server
-    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-    let _ = client.set("127.0.0.1", "0,300".into()).await;
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();// server ip and port here 
+    let formated_ip = format!("{}", local_ip().unwrap());
+    println!("Your local ip is: {}", formated_ip);
+    client.set(&formated_ip, "0,300".into()).await; // player starting position
     // enable_raw_mode().unwrap();
     loop {
-        let start = Instant::now();
-        //--code--
+        let start = Instant::now(); 
         // PUD = Player Update flag
-        // println!("{:?}", client.get("PUD").await.unwrap().unwrap().iter().collect::<Vec<&u8>>());
-        // println!("{:?}", client.get("PUD").await.unwrap().unwrap());
-        let player_pos_raw = client.get("PUD").await.unwrap().unwrap();
+        let player_pos_raw = client.get("PUD").await.unwrap().unwrap();/*looping player updates. 
+        Probably a better way to do this, where the server sends all 
+        clients currently connected updates every time it receives an update 
+        but this is the best I got right now
+        */
         println!("{:?}", player_pos_raw);
-        if poll(std::time::Duration::from_millis(fps - 1)).unwrap() {
+        if poll(std::time::Duration::from_millis(fps - 1)).unwrap() { // This is polling for input waiting for a keypress
             match read().unwrap() {
                 Event::Key(key) => {
-                    if &get_char(key.code).await == &' ' {} else {// Notice this is specifically parsing only when get char returns a char
+                    if &get_char(key.code).await == &' ' {} else { // Notice this is specifically parsing only when get char returns a char
                         println!("Text being sent: {:?}", &get_char(key.code).await);
                         println!("{:?}", client.get(&get_char(key.code).await.to_string()).await.unwrap().unwrap());
+                        // client.get(&get_char(key.code).await.to_string()).await.unwrap().unwrap()
+                        // above is the line of code that will tell the server to change the position based on what key was pressed
                     }
                 }
                 _ => {continue;}
             }
         }
-        //--code--
         let duration = start.elapsed();
-        if duration < Duration::from_millis(fps) {
-            tokio::time::sleep(Duration::from_millis(fps) - duration).await;
+        if duration < Duration::from_millis(fps) { 
+            tokio::time::sleep(Duration::from_millis(fps) - duration).await; // hardsets fps to 60
         }
     }
 }
 
 
-async fn get_char(key: KeyCode) -> char {
+async fn get_char(key: KeyCode) -> char { // converts KeyCode::Char to char, and returns ' ' if not a char
     match key {
         KeyCode::Char(c) => c,
         _ => ' ',
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,7 +4,11 @@ use bytes::Bytes;
 use std::collections::HashMap;
 use std::sync::Arc;
 #[allow(non_camel_case_types)]
-type player_positions = Arc<tokio::sync::Mutex<HashMap<String, Bytes>>>;
+type player_positions = Arc<tokio::sync::Mutex<HashMap<String, Bytes>>>; /*
+I assume you know what a Mutex is, although I'm not sure if Mutex is it's rust specific term
+Arc is a reference counted pointer, which is basically a pointer that can be shared across threads
+Mutex is a guard that prevents multiple threads from accessing the same data at the same time
+*/
 
 #[tokio::main]
 async fn main() {
@@ -13,10 +17,9 @@ async fn main() {
     let player_positions: Arc<tokio::sync::Mutex<HashMap<String, Bytes>>> = Arc::new(tokio::sync::Mutex::new(HashMap::new()));
     loop {
         let (socket, addr) = listener.accept().await.unwrap();
-        // Clone the handle to the hash map.
-        let player_positions = player_positions.clone();
+        let player_positions = player_positions.clone(); // is vector: ip, position
         println!("Accepted from: {}", addr.ip());
-        tokio::spawn(async move {
+        tokio::spawn(async move { // spawns a thread for each client connected
             process(socket, player_positions, addr).await;
         });
     }
@@ -25,7 +28,6 @@ async fn main() {
 async fn process(socket: TcpStream, player_positions: player_positions, sender: std::net::SocketAddr) {
     let screen_size = [300, 300];
     use mini_redis::Command::{self, Get, Set};
-
     // Connection, provided by `mini-redis`, handles parsing frames from
     // the socket
     let mut connection = Connection::new(socket);
@@ -35,21 +37,22 @@ async fn process(socket: TcpStream, player_positions: player_positions, sender: 
         println!("GOT: {:?}", frame);
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                player_positions.insert(sender.ip().to_string(), cmd.value().clone());
-                Frame::Simple("OK".to_string())
+                //TODO: Make sure when setting a player position, it doesn't go out of bounds
+                player_positions.insert(sender.ip().to_string(), cmd.value().clone()); //sets player position based on sender ip
+                Frame::Simple("OK".to_string()) // throwaway response
             }
             Get(cmd) => {
-                if cmd.key() == "PUD" {
+                if cmd.key() == "PUD" { // returns all the player positions
                     let mut player_update = String::new();
                     for (key, value) in player_positions.iter() {
                         player_update.push_str(&format!("{}:{};", key, String::from_utf8(value.to_vec()).unwrap()));
                     }
                     Frame::Bulk(player_update.into())
-                } else if cmd.key() == "w" || cmd.key() == "a" || cmd.key() == "s" || cmd.key() == "d" {
+                } else if cmd.key() == "w" || cmd.key() == "a" || cmd.key() == "s" || cmd.key() == "d" { // handles player movement
                     let mut player_pos_array = split_coords(String::from_utf8(player_positions.get(&sender.ip().to_string()).unwrap().to_vec()).unwrap()).await;
                     println!("Player pos array: {:?}", player_pos_array);
                     let player_movement = player_movement.get(cmd.key()).unwrap();
-                    if player_movement[0] == 1 {
+                    if player_movement[0] == 1 { // this ugly shit is because we have to subtract i8 from u8, which is not allowed. Also makes sure bounds are checked.
                         if player_pos_array[0] < screen_size[0] {
                             player_pos_array[0] += 1;
                         } 
@@ -70,10 +73,10 @@ async fn process(socket: TcpStream, player_positions: player_positions, sender: 
                         }
                     }
                     let player_pos = Bytes::from(format!("{},{}", player_pos_array[0], player_pos_array[1]));
-                    player_positions.insert(sender.ip().to_string(), player_pos.clone());
-                    Frame::Bulk(player_pos.clone())
+                    player_positions.insert(sender.ip().to_string(), player_pos.clone()); // updates database with new player position
+                    Frame::Bulk(player_pos.clone()) // returns new position
                 } else {
-                    println!("here");
+                    // here if get bytes do not match to PUD or directional keys
                     Frame::Null
                 }
             }
@@ -82,11 +85,11 @@ async fn process(socket: TcpStream, player_positions: player_positions, sender: 
         };
 
         // Write the response to the client
-        connection.write_frame(&response).await.unwrap();
+        connection.write_frame(&response).await.unwrap(); 
     }
 }
 
-async fn split_coords(text: String) -> [u16; 2] {
+async fn split_coords(text: String) -> [u16; 2] { // splits bytes like "0,300" into [0, 300]
     let mut split_text = text.split(|x| x == ',');
     for i in split_text.clone() {
         println!("{:?}", i);
