46c116ae07d0dac966306ac318b8d767e0a5a1cd
Jude Packard-Jones <judepackardjones@gmail.com>
2023-12-19 11:34:59 -0500

    |s to singlular get to update position.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,10 +1,9 @@
 use bytes::Bytes;
-use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{poll, read, Event, KeyCode};
 use crossterm::terminal::enable_raw_mode;
 use mini_redis::client;
-use tokio::sync::mpsc;
 use std::time::{Duration, Instant};
-
+#[allow(dead_code)]
 #[derive(Debug)]
 enum Command {
     Get { key: String },
@@ -16,14 +15,7 @@ async fn main() {
     let fps = 60;
     // Establish a connection to the server
     let mut client = client::connect("127.0.0.1:6379").await.unwrap();
-    let first_pos: Bytes = vec![0, 0].into();
-    let second_pos: Bytes = vec![5, 5].into();
-    client.set("P1", first_pos).await.unwrap();
-    client.set("P2", second_pos).await.unwrap();
-    println!(
-        "{}",
-        format!("{:?}", client.get("P1").await.unwrap().unwrap())
-    );
+    let _ = client.set("127.0.0.1", "0,0".into()).await;
     // enable_raw_mode().unwrap();
     loop {
         let start = Instant::now();
@@ -32,14 +24,14 @@ async fn main() {
         // println!("{:?}", client.get("PUD").await.unwrap().unwrap().iter().collect::<Vec<&u8>>());
         // println!("{:?}", client.get("PUD").await.unwrap().unwrap());
         let player_pos_raw = client.get("PUD").await.unwrap().unwrap();
-        let player_positions: Vec<(&str, [u8; 2])> = split_bytes(&player_pos_raw).await;
-        for (player_name, coords) in player_positions {
-            println!("{}: {:?}", player_name, coords);
-        }
+        println!("{:?}", player_pos_raw);
         if poll(std::time::Duration::from_millis(fps - 1)).unwrap() {
             match read().unwrap() {
                 Event::Key(key) => {
-                    change_pos(key.code, "P1", &mut client).await;
+                    if &get_char(key.code).await == &' ' {} else {// Notice this is specifically parsing only when get char returns a char
+                        println!("Text being sent: {:?}", &get_char(key.code).await);
+                        println!("{:?}", client.get(&get_char(key.code).await.to_string()).await.unwrap().unwrap());
+                    }
                 }
                 _ => {continue;}
             }
@@ -52,71 +44,11 @@ async fn main() {
     }
 }
 
-async fn change_pos(key: KeyCode, player_name: &str, client: &mut client::Client) {
-    let current_pos = client.get(player_name).await.unwrap().unwrap();
+
+async fn get_char(key: KeyCode) -> char {
     match key {
-        KeyCode::Char('w') => {
-            if current_pos[1] == 255 {
-                println!("Would go out of bounds");
-                return;
-            } else {
-                client
-                    .set(player_name, vec![current_pos[0], current_pos[1] + 1].into())
-                    .await
-                    .unwrap();
-            }
-        }
-        KeyCode::Char('a') => {
-            if current_pos[0] == 0 {
-                println!("Would go out of bounds");
-                return;
-            } else {
-                client
-                    .set(player_name, vec![current_pos[0] - 1, current_pos[1]].into())
-                    .await
-                    .unwrap();
-            }
-        }
-        KeyCode::Char('s') => {
-            if current_pos[1] == 0 {
-                println!("Would go out of bounds");
-                return;
-            } else {
-                client
-                    .set(player_name, vec![current_pos[0], current_pos[1] - 1].into())
-                    .await
-                    .unwrap();
-            }
-        }
-        KeyCode::Char('d') => {
-            if current_pos[0] == 255 {
-                println!("Would go out of bounds");
-                return;
-            } else {
-                client
-                    .set(player_name, vec![current_pos[0] + 1, current_pos[1]].into())
-                    .await
-                    .unwrap();
-            }
-        }
-        _ => {}
+        KeyCode::Char(c) => c,
+        _ => ' ',
+        
     }
-    println!(
-        "{}, {}",
-        client.get(player_name).await.unwrap().unwrap()[0],
-        client.get(player_name).await.unwrap().unwrap()[1]
-    );
-}
-
-
-async fn split_bytes(bytes: &Bytes) -> Vec<(&str, [u8; 2])> {
-    let mut player_positions: Vec<(&str, [u8; 2])> = Vec::new();
-    let mut split_bytes= bytes.split(|x| x == &b';');
-    for player in split_bytes {
-        let mut split_player = player.split(|x| x == &b':');
-        let player_name = std::str::from_utf8(split_player.next().expect("not enough values")).expect("invalid utf8");
-        let coords: [u8; 2] = [split_player.next().unwrap()[0], split_player.next().unwrap()[0]];
-        player_positions.push((player_name, coords));
-    }
-    player_positions
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -2,56 +2,77 @@ use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection, Frame};
 use bytes::Bytes;
 use std::collections::HashMap;
-use std::sync::{Arc, Mutex};
-
-type Db = Arc<Mutex<HashMap<String, Bytes>>>;
+use std::sync::Arc;
+#[allow(non_camel_case_types)]
+type player_positions = Arc<tokio::sync::Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-
     println!("Listening");
-
-    let db = Arc::new(Mutex::new(HashMap::new()));
+    let player_positions: Arc<tokio::sync::Mutex<HashMap<String, Bytes>>> = Arc::new(tokio::sync::Mutex::new(HashMap::new()));
     loop {
-        let (socket, _) = listener.accept().await.unwrap();
+        let (socket, addr) = listener.accept().await.unwrap();
         // Clone the handle to the hash map.
-        let db = db.clone();
-
-        println!("Accepted");
+        let player_positions = player_positions.clone();
+        println!("Accepted from: {}", addr.ip());
         tokio::spawn(async move {
-            process(socket, db).await;
+            process(socket, player_positions, addr).await;
         });
     }
 }
 
-async fn process(socket: TcpStream, db: Db) {
+async fn process(socket: TcpStream, player_positions: player_positions, sender: std::net::SocketAddr) {
     use mini_redis::Command::{self, Get, Set};
 
     // Connection, provided by `mini-redis`, handles parsing frames from
     // the socket
     let mut connection = Connection::new(socket);
-
+    let player_movement: HashMap<&str, [i8; 2]> = HashMap::from([("w", [0, 1]), ("a", [-1, 0]), ("s", [0, -1]), ("d", [1, 0])]);
+    let mut player_positions = player_positions.lock().await;
     while let Some(frame) = connection.read_frame().await.unwrap() {
         println!("GOT: {:?}", frame);
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                let mut db = db.lock().unwrap();
-                db.insert(cmd.key().to_string(), cmd.value().clone());
+                player_positions.insert(sender.ip().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
-                let db = db.lock().unwrap();
                 if cmd.key() == "PUD" {
                     let mut player_update = String::new();
-                    for (key, value) in db.iter() {
+                    for (key, value) in player_positions.iter() {
                         player_update.push_str(&format!("{}:{};", key, String::from_utf8(value.to_vec()).unwrap()));
                     }
                     Frame::Bulk(player_update.into())
-                }
-                else if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone())
+                } else if cmd.key() == "w" || cmd.key() == "a" || cmd.key() == "s" || cmd.key() == "d" {
+                    let mut player_pos_array = split_coords(String::from_utf8(player_positions.get(&sender.ip().to_string()).unwrap().to_vec()).unwrap()).await;
+                    println!("Player pos array: {:?}", player_pos_array);
+                    let player_movement = player_movement.get(cmd.key()).unwrap();
+                    if player_movement[0] == 1 {
+                        if player_pos_array[0] < 255 {
+                            player_pos_array[0] += 1;
+                        } 
+                    }
+                    if player_movement[0] == -1 {
+                        if player_pos_array[0] > 0 {
+                            player_pos_array[0] -= 1;
+                        }
+                    }
+                    if player_movement[1] == 1 {
+                        if player_pos_array[1] < 255 {
+                            player_pos_array[1] += 1;
+                        } 
+                    }
+                    if player_movement[1] == -1 {
+                        if player_pos_array[1] > 0 {
+                            player_pos_array[1] -= 1;
+                        }
+                    }
+                    let player_pos = Bytes::from(format!("{},{}", player_pos_array[0], player_pos_array[1]));
+                    player_positions.insert(sender.ip().to_string(), player_pos.clone());
+                    Frame::Bulk(player_pos.clone())
                 } else {
+                    println!("here");
                     Frame::Null
                 }
             }
@@ -62,4 +83,12 @@ async fn process(socket: TcpStream, db: Db) {
         // Write the response to the client
         connection.write_frame(&response).await.unwrap();
     }
+}
+
+async fn split_coords(text: String) -> [u8; 2] {
+    let mut split_text = text.split(|x| x == ',');
+    for i in split_text.clone() {
+        println!("{:?}", i);
+    }
+    [split_text.next().unwrap().trim().parse::<u8>().unwrap(), split_text.next().unwrap().trim().parse::<u8>().unwrap()]
 }
